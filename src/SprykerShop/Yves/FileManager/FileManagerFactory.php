7087d6d202b4758f24dcd1f124071f3f2debc02f
Dmitri Kadykov <d.kadykov@gmail.com>
2018-02-16 17:27:07 +0000

    PRYM-117 code style; redundant bundle

--- src/SprykerShop/Yves/FileManager/FileManagerFactory.php
+++ src/SprykerShop/Yves/FileManager/FileManagerFactory.php
@@ -1,21 +1,23 @@
 <?php
 
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
 namespace SprykerShop\Yves\FileManager;
 
-
-use Spryker\Service\FileManager\FileManagerService;
 use Spryker\Yves\Kernel\AbstractFactory;
 
 class FileManagerFactory extends AbstractFactory
 {
-
     /**
-     * @return FileManagerService
+     * @return \Spryker\Service\FileManager\FileManagerService
+     *
      * @throws \Spryker\Yves\Kernel\Exception\Container\ContainerKeyNotFoundException
      */
     public function getFileManagerService()
     {
         return $this->getProvidedDependency(FileManagerDependencyProvider::FILE_MANAGER_SERVICE);
     }
-
 }
